6a00af672266185c50be60dbd64a3552ab330dd1
Jan Scheffler <jan@kjubo.com>
2017-08-08 10:51:57 +0200

    added index.js for easier importing

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+export { default as Store } from './Store';
+export { default as Collection } from './Collection';
+export { default as Context } from './Context';
+
+export { default as DatabaseError } from './errors/DatabaseError';
+export { default as NotFoundError } from './errors/NotFoundError';
+export { default as ValidationError } from './errors/ValidationError';
